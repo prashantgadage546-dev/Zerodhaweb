edb9846f68437ea0d0d6ea3c906dc51e2b792273
prashantgadage546-dev <prashantgadage546@gmail.com>
2025-10-31 16:20:26 +0530

    First commit - my HTML CSS project

---
+++ style.js
@@ -0,0 +1,10 @@
+
+
+var abc = document.getElementById('Fe1')
+console.log('abc');
+console.log(abc.childNodes)
+console.log(abc.children)
+console.log(abc.innerText)
+console.log(abc.style)
+console.log(abc.parentElement)
+console.log(abc.textContent)
